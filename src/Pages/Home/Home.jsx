e7c45ae805719b60152a10b4c14cae8028d9bab8
developerremran <developerremran@gmail.com>
2023-08-02 11:43:25 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import CustomerSuccess from "../../Components/CustomerSuccess/CustomerSuccess";
+import DesignPart from "../../Components/DesignPart/DesignPart";
+import HeaderTitleSection from "../../Components/HeaderTitleSection/HeaderTitleSection";
+import FirstSlide from "../../Components/Slide/FirstSlide";
+import SoftwarePart from "../../Components/SoftwarePart/SoftwarePart";
+import Test from "../../Components/Test/Test";
+
+const Home = () => {
+  return (
+    <div>
+      <HeaderTitleSection></HeaderTitleSection>
+      <DesignPart />
+      <SoftwarePart />
+      <CustomerSuccess />
+    </div>
+  );
+};
+
+export default Home;
